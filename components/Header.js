4fc4affb32ec7994a9c5eee8a6caa962f56449fc
Jacob Colborn <jake@jakesweb.me>
2018-10-28 10:22:25 -0400

    setup the header framework

--- components/Header.js
+++ components/Header.js
@@ -2,21 +2,79 @@
   ./components/Header.js
   header component for pages
 */
+import React from "react";
 import Link from "next/link";
+import Logo from "../svgs/logo.svg";
 
 const linkStyle = {
-  marginRight: 15
+  textDecoration: "none"
 };
 
 const Header = () => (
-  <div>
-    <Link href="/">
-      <a style={linkStyle}>Home</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>About</a>
-    </Link>
-  </div>
+  <header>
+    <div className="fluid-container">
+      <div className="logo-container">
+        <img
+          className="logo-image"
+          src="/static/images/logo.png"
+          alt="Coding Academic Logo"
+        />
+      </div>
+      <nav>
+        <ul className="nav-menu">
+          <li>
+            <Link href="/index">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/courses">
+              <a>Courses</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <style jsx>{`
+      .fluid-container {
+        display: flex;
+        align-items: center;
+        height: 100px;
+        width: 100%;
+      }
+      .logo-container {
+        width: 30%;
+      }
+      .logo-image {
+        width: 100px;
+        height: 100px;
+      }
+      .nav-menu {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+      }
+      .nav-menu li {
+        list-style: none;
+        margin-left: 20px;
+        margin-right: 20px;
+        font-size: 2em;
+      }
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+      a:hover {
+        text-decoration: none;
+        color: inherit;
+      }
+    `}</style>
+  </header>
 );
 
 export default Header;
---
+++ pages/admin.js
@@ -0,0 +1,10 @@
+/*
+  ./pages/admin.js
+  admin route point for admin front end page
+*/
+// node.js imports
+import React from "react";
+// component imports
+import Layout from "../components/Layout";
+
+export default () => <Layout />;
